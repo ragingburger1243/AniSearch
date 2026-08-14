31444f7353d8dfc87c1801e17bd4cf5bb202c4d8
Ragingburger <ragingburger668@gmail.com>
2026-08-14 18:42:42 +0200

    1.3.0
    started working on user search

--- Main.gd
+++ Main.gd
@@ -31,14 +31,20 @@ func _on_button_pressed() -> void:
 	var index = option_button.selected
 	text_option = option_button.get_item_text(index)
 	count_type = "episodes" if text_option == "ANIME" else "chapters"
-	
-	var main_dict = {"query": "query ($name: String) { Page(perPage: 5) { media(search: $name, type: " + text_option + ") { title { romaji english } " + count_type + " averageScore status coverImage { large } description siteUrl genres } } }", "variables":{"name": text}}
 	if text == "":
 		lineEdit.text = "ERROR"
 		return
-		
 	else:
-		httprequest.request("https://graphql.anilist.co", ["Content-Type: application/json"], HTTPClient.METHOD_POST,JSON.stringify(main_dict))
+		if text_option == "USER":
+			var main_dict = {"query": "query ($name: String) { User(name: $name) { name about avatar { large } statistics { anime { count meanScore minutesWatched } manga { count chaptersRead } } } }", "variables": {"name": text}}
+			httprequest.request("https://graphql.anilist.co", ["Content-Type: application/json"], HTTPClient.METHOD_POST,JSON.stringify(main_dict))
+			print(main_dict)
+		else:
+			count_type = "episodes" if text_option == "ANIME" else "chapters"
+			var main_dict = {"query": "query ($name: String) { Page(perPage: 5) { media(search: $name, type: " + text_option + ") { title { romaji english } " + count_type + " averageScore status coverImage { large } description siteUrl genres } } }", "variables":{"name": text}}
+			httprequest.request("https://graphql.anilist.co", ["Content-Type: application/json"], HTTPClient.METHOD_POST,JSON.stringify(main_dict))
+
+		
 
 
 	
@@ -46,13 +52,34 @@ func _on_button_pressed() -> void:
 
 func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
 	var new_body = body.get_string_from_utf8()
-
+	
 	var dict = JSON.parse_string(new_body)
+	
 	item_list.clear()
-	for item in dict["data"]["Page"]["media"]:
-		var title = item["title"]["english"] if item["title"]["english"] != null else item["title"]["romaji"]
-		item_list.add_item(title)
-	results = dict["data"]["Page"]["media"]
+	if text_option == "USER":
+		var about_old = dict["data"]["User"]["about"]
+		var about = ""
+		if about_old != null:
+			var regex = RegEx.new()
+			regex.compile("<[^>]+>")
+			about = regex.sub(about_old, "", true)
+		else:
+			label_score.text = "No bio"
+			
+		label_episodes.text = "USERNAME: " + str(dict["data"]["User"]["name"])
+		label_score.text = "ABOUT: " +  str(about)
+		label_status.text = "ANIME COUNT: " + str(dict["data"]["User"]["statistics"]["anime"]["count"])
+		description_text.text = "MINUTES WATCHED: " + str(dict["data"]["User"]["statistics"]["anime"]["minutesWatched"]) + " Minutes"
+		image = dict["data"]["User"]["avatar"]["large"]
+		label_genres.text ="MANGA COUNT: " +  str(dict["data"]["User"]["statistics"]["manga"]["count"])
+
+		httprequestimage.request(image)
+	else:
+		for item in dict["data"]["Page"]["media"]:
+			var title = item["title"]["english"] if item["title"]["english"] != null else item["title"]["romaji"]
+			item_list.add_item(title)
+	
+		results = dict["data"]["Page"]["media"]
 	
 
 
@@ -77,7 +104,7 @@ func _on_item_list_item_selected(index: int) -> void:
 		if results[index]["title"]["english"] != null:
 			label_title.text =  "ENGLISH TITLE: " + results[index]["title"]["english"]
 		else:
-			label_title.text =  "ENGLISH TITLE: " + results[index]["title"]["romaji"]
+			label_title.text =  "ROMAJI TITLE: " + results[index]["title"]["romaji"]
 		label_episodes.text =  count_type.to_upper() + " COUNT: " + str(results[index][count_type])
 		label_score.text = "SCORE:  " + str(results[index]["averageScore"])
 		label_status.text = "STATUS: " + str(results[index]["status"])
